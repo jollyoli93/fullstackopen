15f173ee2ebbb31d9efefe58dfb7d9139f0e41a8
jollyoli93 <jackoliver1993@yahoo.co.uk>
2024-08-19 20:12:07 +0100

    part1.1

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,54 @@
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  function Header ( {course} ) {
+    return (
+      <>
+        <h1>{course}</h1>
+      </>
+    )
+  }
+
+  function Content ( {part, exercises } ) {
+    return (
+      <>
+        <p>
+          {part[0]} {exercises[0]}
+        </p>
+        <p>
+          {part[1]} {exercises[1]}
+        </p>
+        <p>
+          {part[2]} {exercises[2]}
+        </p>
+      </>
+    )
+  }
+
+  function Total ( {exercises} ) {
+    return (
+      <>
+        <p>Number of exercises {exercises[0] + exercises[1] + exercises[2]}</p>
+      </>
+    )
+  }
+
+
+  return (
+    <div>
+        <Header course={course} />
+        <Content part={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]}/>
+        <Total exercises={[exercises1, exercises2, exercises3]}/>
+    </div>
+  )
+}
+
+export default App
+
+
