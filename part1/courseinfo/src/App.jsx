98fd6b04f6abd7567c264f54910c140f36f1d5a7
jollyoli93 <jackoliver1993@yahoo.co.uk>
2024-08-20 20:32:32 +0100

    components moved outside of app

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,3 +1,38 @@
+
+const Part = ( {part} ) => {
+  return (
+    <>
+      <p>{part[0].name} {part[0].exercises}</p>
+      <p>{part[1].name} {part[1].exercises}</p>
+      <p>{part[2].name} {part[2].exercises}</p>
+    </>
+  )
+}
+
+const Header = ( {course} ) => {
+  return (
+    <>
+      <h1>{course}</h1>
+    </>
+  )
+}
+
+const Content = ({part}) => {
+  return (
+    <div>
+      <Part part={part}/>
+    </div>
+  )
+}
+
+const Total = ( {part} ) => {
+  return (
+    <>
+      <p>Number of exercises {part[0].exercises + part[1].exercises + part[2].exercises}</p>
+    </>
+  )
+}
+
 const App = () => {
   const course = {
     name: 'Half Stack application development',
@@ -16,41 +51,6 @@ const App = () => {
       }
     ]
   }
-
-  const Part = ( {part} ) => {
-    return (
-      <>
-        <p>{part[0].name} {part[0].exercises}</p>
-        <p>{part[1].name} {part[1].exercises}</p>
-        <p>{part[2].name} {part[2].exercises}</p>
-      </>
-    )
-  }
-
-  const Header = ( {course} ) => {
-    return (
-      <>
-        <h1>{course}</h1>
-      </>
-    )
-  }
-
-  const Content = ({part}) => {
-    return (
-      <div>
-        <Part part={part}/>
-      </div>
-    )
-  }
-
-  const Total = ( {part} ) => {
-    return (
-      <>
-        <p>Number of exercises {part[0].exercises + part[1].exercises + part[2].exercises}</p>
-      </>
-    )
-  }
-
 
   return (
     <div>
